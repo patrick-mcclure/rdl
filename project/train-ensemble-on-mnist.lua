12d800cef546b140c13be7a88613fc2d8b26adc2
patrick-mcclure <patrick.s.mcclure@gmail.com>
2015-03-11 14:39:54 +0000

    Codded and ran more RDM tests

--- project/train-ensemble-on-mnist.lua
+++ project/train-ensemble-on-mnist.lua
@@ -193,7 +193,7 @@ function train(dataset)
    print("<trainer> online epoch # " .. epoch .. ' [batchSize = ' .. opt.batchSize .. ']')
    for t = 1,dataset:size(),opt.batchSize do
              
-      if(batchCounter%100 == 0) then 
+      if(batchCounter%50 == 0) then 
         local rdm = rdl.createSSRDM(model,trainRDL,{3,6,10})
         torch.save('rdms/rdm_' .. opt.model_num .. '_' .. nRDLTrain .. '_' .. batchCounter .. '.t7',rdm)
         fbmat.save('rdms/rdm_' .. opt.model_num .. '_' .. nRDLTrain .. '_' .. batchCounter .. '.mat',rdm)
